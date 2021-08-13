3368b2ea8226332b79ef26d0a4a9c50950fa5c72
Cathy Richardson <cathyanne08@gmail.com>
2021-08-12 20:14:09 -0600

    completed step 3

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const massive = require('massive')
 require('dotenv').config()
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING} = process.env;
 
 const app = express();
 
 app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+})
+.then((dbInstance) => {
+    app.set('db', dbInstance);
+})
+.catch(e => console.log(e));
 
 app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`) );
